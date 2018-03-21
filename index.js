a7eebffc025847b7957bde0b990e02c9f9a0d2e1
Ozan Döğüşcü <doguscu_12@hotmail.com>
2018-03-21 15:29:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -27,36 +27,8 @@ function viewCart() {
     return 'Your shopping cart is empty.';
 
   } else if (cart.length === 1) {
-    var product = cart[0];
-    return 'In your cart, you have ' + Object.keys(product) + " at $" + product[Object.keys(product)] + '.';
-
-    } else if (cart.length > 1) {
-        for (var i = 0; i < cart.length; i++) {
-          var item = cart[i];
-          var itemName = Object.keys(item);
-
-          if (i === cart.length - 1) {
-            if (cart.length === 2) {
-              var words = message.split(' ');
-              var lastEle = words[words.length - 1];
-              var chars = lastEle.split('');
-              var newEle = chars.slice(0,chars.length - 1).join('');
-              message = message.split(lastEle).join(newEle);
-              message += ' and ' + itemName + " at $" + item[itemName] + '.';
-
-            } else {
-              message += ' and ' + itemName + " at $" + item[itemName] + '.';
-            }
-          }
-
-          else {
-            message += ' ' + itemName + " at $" + item[itemName] + ',';
-          }
-        }
-      }
-
-  return console.log(message);
-}
+    return "In your cart, you have " + Object.keys(product) + " at $" + product[Object.keys(product)] + ".";
+  }
 
 function total() {
   // write your code here
