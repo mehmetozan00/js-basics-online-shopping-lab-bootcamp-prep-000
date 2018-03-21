3edb1f69a4ee04dc6e884ac596416725f618ff34
Ozan Döğüşcü <doguscu_12@hotmail.com>
2018-03-21 15:54:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ function setCart(c) {
 }
 
 function addToCart(item) {
-  var price = Math.floor(Math.random() * 100);
   var item_objects = {};
+  var price = Math.floor(Math.random() * 100);
 
   item_objects.itemName = item;
   item_objects.itemPrice = price;
@@ -21,17 +21,22 @@ function addToCart(item) {
 }
 
 function viewCart() {
-  var message = 'In your cart, you have ';
-
   if (cart.length === 0) {
-    return 'Your shopping cart is empty.';
-
-  } else if (cart.length === 1) {
-    var product = cart[0];
-    message += Object.keys(cart) + " at $" + cart[Object.keys(cart)] + '.';
-    return message;
+    return "Your shopping cart is empty.";
   }
-}
+
+  var cartString = 'In your cart, you have';
+  for (var i = 0; i < cart.length; i++) {
+    if (i === cart.length - 1 && cart.length !== 1) {
+      cartString += ` and ${cart[i].itemName} at $${cart[i].itemPrice}.`;
+    } else if (cart.length === 1) {
+      cartString += ` ${cart[i].itemName} at $${cart[i].itemPrice}.`;
+    } else {
+      cartString += ` ${cart[i].itemName} at $${cart[i].itemPrice},`;
+    }
+  }
+  return cartString;
+  }
 
 function total() {
   // write your code here
