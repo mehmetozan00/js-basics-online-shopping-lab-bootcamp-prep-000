35a118d8f00d1a3d8ac63f570145f20f9132a4d0
Ozan Döğüşcü <doguscu_12@hotmail.com>
2018-03-21 15:35:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,10 +26,6 @@ function viewCart() {
   if (cart.length === 0) {
     return 'Your shopping cart is empty.';
 
-  } else if (cart.length === 1) {
-
-  }
-
 function total() {
   // write your code here
 }
