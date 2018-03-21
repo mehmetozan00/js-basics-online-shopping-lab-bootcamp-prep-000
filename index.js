a586c27b5d7e975cbbb6e19ede0d62d262709a66
Ozan Döğüşcü <doguscu_12@hotmail.com>
2018-03-21 15:20:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,11 +10,48 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var price = Math.floor(Math.random() * 100);
+  var item_objects = {};
+
+  item_objects.itemName = item;
+  item_objects.itemPrice = price;
+
+  cart.push(item_objects);
+  return item + " has been added to your cart.";
 }
 
 function viewCart() {
-  // write your code here
+  var message = 'In your cart, you have ';
+
+  if (cart.length === 0) {
+    return 'Your shopping cart is empty.';
+  } else if (cart.length === 1) {
+    var product = cart[0];
+    message += Object.keys(product) + " at $" + product[Object.keys(product)] + '.';
+    return message;
+    } else if (cart.length > 1) {
+        for (var i = 0; i < cart.length; i++) {
+          var item = cart[i];
+          var itemName = Object.keys(item);
+
+          if (i === cart.length - 1) {
+            if (cart.length === 2) {
+            var words = message.split(' ');
+            var lastEle = words[words.length - 1];
+            var chars = lastEle.split('');
+            var newEle = chars.slice(0,chars.length - 1).join('');
+            message = message.split(lastEle).join(newEle);
+            message += ' and ' + itemName + " at $" + item[itemName] + '.';
+            } else {
+              message += ' and ' + itemName + " at $" + item[itemName] + '.';
+            }
+          } else {
+            message += ' ' + itemName + " at $" + item[itemName] + ',';
+          }
+        }
+      }
+
+  return console.log(message);
 }
 
 function total() {
