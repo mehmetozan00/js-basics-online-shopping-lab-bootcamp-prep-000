b7c7d4773cb6a5376838aa514773087844d3c438
Ozan Döğüşcü <doguscu_12@hotmail.com>
2018-03-21 15:35:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,10 +21,7 @@ function addToCart(item) {
 }
 
 function viewCart() {
-  var message = 'In your cart, you have ';
 
-  if (cart.length === 0) {
-    return 'Your shopping cart is empty.';
 
 function total() {
   // write your code here
